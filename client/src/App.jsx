ee43bb65474b3aa4e59f64c0f25b6e53dfb681d1
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-08-21 15:57:13 +0100

    Add create place route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles/style.scss';
 import { Switch, Route, Link } from 'react-router-dom';
 import HomeView from './views/HomeView';
+import CreatePlace from './views/CreatePlace';
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path="/place/create" exact component={CreatePlace} />
         <Route path="/" exact component={HomeView} />
       </Switch>
     </div>
