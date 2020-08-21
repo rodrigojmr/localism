06e6caf8954ee127fd1814a1060a0be46e1c9f23
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-08-21 14:47:32 +0100

    Merge HomeView

--- client/src/views/HomeView.jsx
+++ client/src/views/HomeView.jsx
@@ -20,12 +20,8 @@ class HomeView extends Component {
   render() {
     return (
       <div className="home">
-<<<<<<< HEAD
         <h1>Home View</h1>
         <Map />
-=======
-        <PlaceForm />
->>>>>>> 279c65d131ea4ebc8712fb626e49c4edd85331ae
         <PlacesList />
         <Link to="/place/create">Create Place</Link>
       </div>
