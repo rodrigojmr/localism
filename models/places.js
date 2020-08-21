c22bfb6ee150f85449ff4ee3dc27af2c0fa51e76
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-08-21 01:47:47 +0100

    Places model

---
+++ models/places.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String
+    },
+    openDate: {
+      type: Date
+    },
+    schedule: [
+      {
+        weekday: {
+          type: String
+        },
+        time: {
+          opening: {
+            type: Date
+          },
+          closing: {
+            type: Date
+          }
+        }
+      }
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    contacts: {
+      phoneNumber: {
+        type: String
+      }
+    },
+    locationName: {
+      type: String,
+      required: true
+    },
+    location: {
+      coordinates: [
+        {
+          type: Number,
+          min: -180,
+          max: 180
+        }
+      ],
+      type: {
+        type: String,
+        default: 'Point'
+      }
+    },
+    suggestions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Suggestion'
+      }
+    ]
+  },
+  {
+    timestamps: {
+      createdAt: 'creationDate',
+      updatedAt: 'editDate'
+    }
+  }
+);
+
+const Place = mongoose.model('Place', placeSchema);
+
+module.exports = Place;
