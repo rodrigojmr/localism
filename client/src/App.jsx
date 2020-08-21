29e109ef780581f9dc14e9a0dfdf8a7dbf9e8c7f
bbrolezzi <bbrolezzi@users.noreply.github.com>
2020-08-21 17:57:07 +0100

    transformed App in a class base component and adjusted Sign In

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/style.scss';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { loadProfile, signOut } from './services/authentication';
 import HomeView from './views/HomeView';
 import AuthenticationSignInView from './views/Authentication/SignInView';
 
 import Navbar from './components/Navbar';
-import ProtectedRoute from './components/ProtectedRoute';
+//import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
-  return (
-    <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={HomeView} />
-          <Route
-            path='/authentication/sign-in'
-            exact
-            component={AuthenticationSignInView}
-          />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loaded: false,
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    loadProfile()
+      .then(data => {
+        const user = data.user;
+        this.handleUserUpdate(user);
+        this.setState({
+          loaded: true
+        });
+      })
+      .then(error => {
+        console.log(error);
+      });
+  }
+
+  handleUserUpdate = user => {
+    this.setState({
+      user
+    });
+  };
+
+  handleSignOut = () => {
+    signOut()
+      .then(() => {
+        this.handleUserUpdate(null);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <BrowserRouter>
+          <Navbar user={this.state.user} onSignOut={this.handleSignOut} />
+          <Switch>
+            <Route path='/' component={HomeView} exact />
+            <Route
+              path='/authentication/sign-in'
+              component={AuthenticationSignInView}
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
