f393da7b8ef21d18471df9d939aba8957240e6a5
Diana Odiaka-Obiagwu <59796234+dianabeth@users.noreply.github.com>
2020-08-21 14:48:22 +0100

    fixed homeview

--- client/src/components/Form/PlaceForm.jsx
+++ client/src/components/Form/PlaceForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+//import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 export const PlaceForm = props => {
   const handleFormSubmission = event => {
--- client/src/views/HomeView.jsx
+++ client/src/views/HomeView.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Map from '../components/Map';
 import PlacesList from '../components/List/PlacesList';
-import PlaceForm from '../components/Form/PlaceForm';
 
 class HomeView extends Component {
   constructor(props) {
@@ -20,12 +19,8 @@ class HomeView extends Component {
   render() {
     return (
       <div className="home">
-<<<<<<< HEAD
         <h1>Home View</h1>
         <Map />
-=======
-        <PlaceForm />
->>>>>>> 279c65d131ea4ebc8712fb626e49c4edd85331ae
         <PlacesList />
         <Link to="/place/create">Create Place</Link>
       </div>
