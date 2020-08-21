f277e07d592efbd37eb2993c6202d05eea299ef1
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-08-21 12:20:02 +0100

    Change route authentication guard

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,14 +1,11 @@
 'use strict';
 
-// Route Guard Middleware
-// This piece of middleware is going to check if a user is authenticated
-// If not, it sends the request to the app error handler with a message
-module.exports = (req, res, next) => {
-  if (req.user) {
-    next();
+module.exports = authenticated => (req, res, next) => {
+  if (authenticated && !req.user) {
+    next(new Error('NO_PERMISSION'));
+  } else if (!authenticated && req.user) {
+    next(new Error('USER_MUST_BE_UNAUTHENTICATED'));
   } else {
-    const error = new Error('User is not authenticated');
-    error.status = 401;
-    next(error);
+    next();
   }
 };
