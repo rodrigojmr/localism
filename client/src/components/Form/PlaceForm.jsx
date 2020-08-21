fbafe69af609cbe471a5114caf7aba6ac1f2caff
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-08-21 17:46:39 +0100

    Added datepicker, proptypes, momentjs

--- client/src/components/Form/PlaceForm.jsx
+++ client/src/components/Form/PlaceForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DatePicker from 'react-datepicker';
 
 //import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
@@ -16,6 +17,83 @@ export const PlaceForm = props => {
 
   return (
     <form className="form form-create-place" onSubmit={onFormSubmission}>
+      <div className="input-group">
+        <label htmlFor="input-name">name</label>
+        <input
+          placeholder="Place name"
+          type="text"
+          name="name"
+          id="input-name"
+          value={props.address}
+          onChange={onvalueChange}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="input-address">category</label>
+        <input
+          placeholder="Place category"
+          type="text"
+          name="category"
+          id="input-category"
+          value={props.category}
+          onChange={onvalueChange}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="input-open-date">Open Date</label>
+        <input
+          placeholder="Open Since..."
+          type="date"
+          name="openDate"
+          id="input-open-date"
+          value={props.openDate}
+          onChange={onvalueChange}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="input-address">Address</label>
+        <input
+          placeholder="Place address"
+          type="text"
+          name="address"
+          id="input-address"
+          value={props.address}
+          onChange={onvalueChange}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="input-address">Address</label>
+        <input
+          placeholder="Place address"
+          type="text"
+          name="address"
+          id="input-address"
+          value={props.address}
+          onChange={onvalueChange}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="input-address">Address</label>
+        <input
+          placeholder="Place address"
+          type="text"
+          name="address"
+          id="input-address"
+          value={props.address}
+          onChange={onvalueChange}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="input-address">Address</label>
+        <input
+          placeholder="Place address"
+          type="text"
+          name="address"
+          id="input-address"
+          value={props.address}
+          onChange={onvalueChange}
+        />
+      </div>
       <div className="input-group">
         <label htmlFor="input-address">Address</label>
         <input
