b90e0c8840f95fb711209a5cacdaad3a5d9dd7bf
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-08-24 17:00:55 +0100

    Preparation for handling avatar in API

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -8,6 +8,14 @@ const api = axios.create({
 export const signUp = body =>
   api.post('/sign-up', body).then(response => response.data);
 
+// export const signUp = body => {
+//   const formBody = new window.FormData();
+//   formBody.append('content', body.content);
+//   formBody.append('photo', body.photo);
+//   // for (let property in body) formBody.append(property, body[property]);
+//   api.post('/post', formBody).then(response => response.data);
+// };
+
 export const signIn = body =>
   api.post('/sign-in', body).then(response => response.data);
 
