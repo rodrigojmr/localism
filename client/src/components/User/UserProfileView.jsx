0ea07355c80eb083486d8df42fe9c167d4af8d6a
Diana Odiaka-Obiagwu <59796234+dianabeth@users.noreply.github.com>
2020-08-26 12:57:20 +0100

    Create profile edit form

---
+++ client/src/components/User/UserProfileView.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+import SignUpForm from '../../components/User/SignUpForm';
+
+export class UserProfileView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+      gender: '',
+      birthday: '',
+      avatarPreview: '/images/default-avatar.png',
+      avatar: '',
+      location: undefined,
+      address: undefined
+    };
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          location: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+        });
+      },
+      function error(msg) {
+        alert('Please enable your GPS position feature.');
+      },
+      { maximumAge: 10000, timeout: 5000, enableHighAccuracy: true }
+    );
+  }
+
+  handleValueChange = (name, value) => {
+    this.setState({
+      [name]: value
+    });
+  };
+
+  emailValidation = email => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  };
+
+  handleFormSubmission = () => {
+    const body = { ...this.state };
+    signUp(body)
+      .then(data => {
+        const { user } = data;
+        this.props.onUserUpdate(user);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  handleAvatarInputChange = avatar => {
+    this.setState({
+      avatarPreview: URL.createObjectURL(avatar),
+      avatar
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <SignUpForm
+          {...this.state}
+          onDateChange={this.handleDateChange}
+          onValueChange={this.handleValueChange}
+          onAvatarChange={this.handleAvatarInputChange}
+          onFormSubmission={this.handleFormSubmission}
+        />
+      </div>
+    );
+  }
+}
+
+export default UserProfileView;
---
+++ client/src/views/User/UserProfile.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { loadProfile } from './../../services/profile';
+//import SinglePlaceMap from './../../components/Map/SinglePlaceMap';
+import getHours from 'date-fns/getHours';
+
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      user: undefined
+    };
+  }
+
+  handleContentChange() {}
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    loadProfile(id).then(data => {
+      const { user } = data;
+      this.setState({
+        loaded: true,
+        user
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="user">
+        {this.state.loaded && (
+          <>
+            <div className="user-info__row">
+              <div>
+                <h1>{user.name}</h1>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
