5dbdfc93b6a43f6ad8e4bbceeb428417cf70d1b0
bbrolezzi <bbrolezzi@users.noreply.github.com>
2020-08-26 21:39:07 +0100

    commit

--- client/src/views/Authentication/SignInView.jsx
+++ client/src/views/Authentication/SignInView.jsx
@@ -37,39 +37,45 @@ class AuthenticationSignInView extends Component {
 
   render() {
     return (
-      <div className="sign-in-page">
+      <div className='sign-in-page'>
+        <div className='sign-in-title'>
+          <h1>Ready to get</h1>
+          <br></br>
+          <h1>Local?</h1>
+        </div>
         <form onSubmit={this.handleFormSubmission}>
-          <label htmlFor="input-username">Username</label>
-          <input
-            id="input-username"
-            type="text"
-            name="username"
-            placeholder="username"
-            value={this.state.username}
-            onChange={this.handleInputChange}
-            required
-          />
-
-          <label htmlFor="input-password">Password</label>
-          <input
-            id="input-password"
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-            required
-            minLength="8"
-          />
-
+          <div className='input-group'>
+            <label htmlFor='input-username'>Username</label>
+            <input
+              id='input-username'
+              type='text'
+              name='username'
+              placeholder='username'
+              value={this.state.username}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+          <div className='input-group'>
+            <label htmlFor='input-password'>Password</label>
+            <input
+              id='input-password'
+              type='password'
+              name='password'
+              placeholder='Password'
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              required
+              minLength='8'
+            />
+          </div>
           {this.state.error && (
-            <div className="error-block">
+            <div className='error-block'>
               <p>There was an error submiting the form:</p>
               <p>{this.state.error.message}</p>
             </div>
           )}
-
-          <button>Sign In</button>
+          <button className='sign-button'>Sign In</button>
         </form>
       </div>
     );
