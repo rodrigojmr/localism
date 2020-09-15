7dadb283b6f18a818a2a0d22300ef864422b02c5
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-09-15 13:35:08 +0100

    User profile progress

--- client/src/views/User/UserProfile.jsx
+++ client/src/views/User/UserProfile.jsx
@@ -32,98 +32,64 @@ class UserProfile extends Component {
     const { publicUser } = this.state;
     const { place } = this.state;
     return (
-      <div className="form">
+      <div className="profile-page">
         {this.state.loaded && (
           <>
-            <div className="user-profile-picture">
-              <div className="image-cropper">
-                <Link to={`/profile/${publicUser._id}`}>
-                  <img
-                    src={publicUser.avatar}
-                    alt={publicUser.name}
-                    className="profile-pic"
-                  />
-                </Link>
-              </div>
-              <div className="input-username">
-                <div className="fullname">
-                  <p>Username</p>
-                  <h4>
-                    <strong> {publicUser.username}</strong>
-                  </h4>
+            <main className="user-profile">
+              <img
+                src={publicUser.avatar}
+                alt={publicUser.name}
+                className="user-profile__avatar u-border-radius-50"
+              />
+              <h1 className="heading heading--1 u-margin-bottom-xsmall">
+                {publicUser.name}
+              </h1>
+              <p>{publicUser.info?.about}</p>
+
+              <h1 className="heading heading--1">From {publicUser.locality}</h1>
+              <div className="user-profile__info">
+                <div className="user-profile__info-item">
+                  Supports:
+                  <br />
+                  {publicUser.supports.length} Places
                 </div>
               </div>
-            </div>
 
-            <div className="fullname">
-              <p>FullName</p>
-              <h4>
-                <strong> {publicUser.name}</strong>
-              </h4>
-            </div>
-            <div className="fullname">
-              <p>Email</p>
-              <h4>
-                <strong> {publicUser.email}</strong>
-              </h4>
-            </div>
-            <div className="fullname">
-              <p> Gender</p>
-              {(publicUser.gender && publicUser.gender.length > 0 && (
-                <h4>
-                  <strong> {publicUser.gender}</strong>
-                </h4>
-              )) || <h4>No Gender Specified.</h4>}
-            </div>
-            <div className="fullname">
-              <p>Birthday</p>
-              {(publicUser.birthday && (
-                <h4>
-                  <strong> {publicUser.info.birthday}</strong>
-                </h4>
-              )) || <h4>No Birthdate Specified.</h4>}
-            </div>
+              <div>
+                {publicUser.owner && (
+                  <h4>
+                    Owns:
+                    <Link to={`/place/${place._id}`}>
+                      {' '}
+                      {place.avatar} {place.name}
+                    </Link>
+                  </h4>
+                )}
+              </div>
+              <div className="user-supports">
+                {publicUser.supports.length && (
+                  <>
+                    <h2> You Support:</h2>
+                    <div className="user-supports-wrapper">
+                      {publicUser.supports.map(support => (
+                        <div className="support-small" key={support._id}>
+                          <Link to={`/profile/${support.place._id}`}>
+                            <img
+                              src={support.place.images[0]}
+                              alt={support.place.name}
+                            />
+                          </Link>
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                )}
+              </div>
 
-            <div className="fullname">
-              <p>You are based at:</p>
-              <h4>
-                <strong> {this.state.publicUser.locality}</strong>
-              </h4>
-            </div>
-            <div>
-              {publicUser.owner && (
-                <h4>
-                  Owns:
-                  <Link to={`/place/${place._id}`}>
-                    {' '}
-                    {place.avatar} {place.name}
-                  </Link>
-                </h4>
-              )}
-            </div>
-            <div className="user-supports">
-              {publicUser.supports.length && (
-                <>
-                  <h2> You Support:</h2>
-                  <div className="user-supports-wrapper">
-                    {publicUser.supports.map(support => (
-                      <div className="support-small" key={support._id}>
-                        <Link to={`/profile/${support.place._id}`}>
-                          <img
-                            src={support.place.images[0]}
-                            alt={support.place.name}
-                          />
-                        </Link>
-                      </div>
-                    ))}
-                  </div>
-                </>
-              )}
-            </div>
-
-            <Link to={'/profile/edit'}>
-              <button className="sign-button"> Edit Profile</button>
-            </Link>
+              <Link to={'/profile/edit'}>
+                <button className="sign-button"> Edit Profile</button>
+              </Link>
+            </main>
           </>
         )}
       </div>
