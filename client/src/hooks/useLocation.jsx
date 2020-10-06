8c9c546f1b4fcced67ff52ad1109750433840a4a
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-10-06 19:25:56 +0100

    useLocation custom hook

---
+++ client/src/hooks/useLocation.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+const useLocation = () => {
+  const [location, setLocation] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const geo = naigator.geolocation;
+    if (!geo) {
+      setError('Geolocation is not supported');
+      return;
+    }
+
+    const watcher = geo.watchPosition(onChange, onError, options);
+
+    return () => geo.clearWatch(watcher);
+  }, []);
+
+  const onError = error => {
+    setError(error.message);
+  };
+
+  const onChange = ({ coords }) => {
+    setLocation({
+      lat: coords.latitude,
+      lng: coords.longitude
+    });
+  };
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  return { ...location, error };
+};
+
+export default useLocation;
